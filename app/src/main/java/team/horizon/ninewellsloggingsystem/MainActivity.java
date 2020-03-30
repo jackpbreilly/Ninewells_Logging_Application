d3aa7f4920910924fd8cc23ccc114aa233cba8ee
jackpbreilly <jackpbreilly@gmail.com>
2020-03-30 15:17:28 +0100

    adds data to fields

--- app/src/main/java/team/horizon/ninewellsloggingsystem/MainActivity.java
+++ app/src/main/java/team/horizon/ninewellsloggingsystem/MainActivity.java
@@ -1,5 +1,6 @@
 package team.horizon.ninewellsloggingsystem;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -15,6 +16,7 @@ import android.graphics.Rect;
 import android.graphics.pdf.PdfDocument;
 import android.graphics.pdf.PdfRenderer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,17 +26,20 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
 import com.itextpdf.text.Annotation;
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfCopy;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.PushbuttonField;
 
 import org.w3c.dom.Text;
 
@@ -42,17 +47,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static com.itextpdf.text.Image.getInstance;
+
 public class MainActivity extends AppCompatActivity {
 
     Button submitBtn, signBtn;
     Spinner formSpinner;
-    EditText editText;
+
+    EditText jobNo, name;
 
     String[] permissions = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -66,9 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
         checkPermissions();
         initializeUI();
-        populateSpinner();
+        //populateSpinner();
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
                 try {
@@ -91,56 +102,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void addInputToForm(String copyFormPath) throws IOException, DocumentException {
         PdfReader readerOriginalDoc = new PdfReader(copyFormPath);
         PdfStamper stamper = new PdfStamper(readerOriginalDoc,new FileOutputStream("/sdcard/Download/newStamper.pdf"));
-        PdfContentByte content = stamper.getOverContent(1);
-        Image image = Image.getInstance("/sdcard/Download/last_sig.bmp");
-        image.scaleAbsolute(500, 200);
-        image.setAbsolutePosition(20, 30);
-        image.setAnnotation(new Annotation(0, 0, 0, 0, 3));
-        content.addImage(image);
+        AcroFields form = stamper.getAcroFields();
+        form.setField("jobId", String.valueOf(jobNo.getText()));
+        form.setField("Name", String.valueOf(name.getText()));
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDate localDate = LocalDate.now();
+
+        form.setField("Date", String.valueOf(dtf.format(localDate)));
+        PushbuttonField signature = form.getNewPushbuttonFromField("Signature");
+        signature.setLayout(PushbuttonField.LAYOUT_ICON_ONLY);
+        signature.setProportionalIcon(true);
+        signature.setImage(Image.getInstance("/sdcard/Download/last_sig.bmp"));
+        form.replacePushbuttonField("Signature", signature.getField());
         stamper.close();
     }
 
 
-    private void createPdf2(){
-        Bitmap bitmap = BitmapFactory.decodeFile("/sdcard/Download/last_sig.bmp");
-
-
-        PdfDocument document = new PdfDocument();
-
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 1).create();
-
-        PdfDocument.Page page = document.startPage(pageInfo);
-        Canvas canvas = page.getCanvas();
-        Paint paint = new Paint();
-        Rect rectangle = new Rect(0,0,100,100);
-        canvas.drawBitmap(bitmap, null, rectangle, paint);
-        document.finishPage(page);
-
-        String directory_path = "/sdcard/Download";
-        File file = new File(directory_path);
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        String targetPdf = directory_path+"/form" + Calendar.getInstance().getTime()+".pdf";
-        File filePath = new File(targetPdf);
-        try {
-            document.writeTo(new FileOutputStream(filePath));
-            Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            Log.e("main", "error "+e.toString());
-            Toast.makeText(this, "Something wrong: " + e.toString(),  Toast.LENGTH_LONG).show();
-        }
-        // close the document
-        document.close();
-    }
 
     private void initializeUI() {
         submitBtn = findViewById(R.id.submit);
         signBtn = findViewById(R.id.sign);
         formSpinner = findViewById(R.id.form);
+        jobNo = findViewById(R.id.jobNo);
+        name = findViewById(R.id.userName);
     }
 
     private boolean checkPermissions() {
